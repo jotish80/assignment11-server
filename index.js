a76605b3c943fca51ef0f5dd741831cf04b5066f
jotish80 <sjotishchandra@gmail.com>
2020-10-15 16:06:48 +0200

    modified database

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { ObjectId } = require("mongodb");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config;
+const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
+// app.use(express.static("services"));
+// app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("hello from database");
 });
 const uri =
-  "mongodb+srv://MyProjects:yesicanmakeit40@cluster0.o0xk6.mongodb.net/volunteerdb?retryWrites=true&w=majority";
+  "mongodb+srv://CreativeAgency:Project11@cluster0.o0xk6.mongodb.net/awesome?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 client.connect((err) => {
-  console.log("mongodb connected");
+  const ordersCollection = client.db("awesome").collection("orders");
+  const reviewsCollection = client.db("awesome").collection("reviews");
+  const servicesCollection = client.db("awesome").collection("services");
+  const adminCollection = client.db("awesome").collection("admins");
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    orderCollection.insertOne(order).then((result) => {
+      res.send(result.insertCount > 0);
+    });
+  });
 });
 
 app.listen(process.env.PORT || port);
